9f927f10573f4a4e3c4c5656840b76285a56fb0d
bwestberg <Bwestberg@live.com>
2026-09-20 17:23:57 -0400

    Create master.js to aggregate people from modules

---
+++ data/master.js
@@ -0,0 +1,11 @@
+import { westberg } from "./westberg.js";
+import { losie } from "./losie.js";
+import { lakin } from "./lakin.js";
+import { sawyer } from "./sawyer.js";
+
+export const people = [
+  ...westberg,
+  ...losie,
+  ...lakin,
+  ...sawyer
+];
