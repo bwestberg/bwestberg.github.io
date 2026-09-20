128e92edf755041ee97c749501fafba62ada2477
bwestberg <Bwestberg@live.com>
2026-09-20 17:58:41 -0400

    Refactor Losie family tree data structure
    Updated family tree entries for the Losie lineage, including changes to IDs, names, and relationships. Removed several historical entries and added new ones with updated details.

--- data/losie.js
+++ data/losie.js
@@ -1,217 +1,137 @@
 export const losie = [
-  // 🔹 Deep Losee / Koeck / VanFlaesbeck / Denton line
-
-  {
-    id: "laurens-koeck",
-    name: "Laurens Corneliszen Koeck (1640–1702)",
-    branch: "Losie",
-    parents: [],
-    spouses: ["margrietje-vanflaesbeck"],
-    children: ["grietje-kouk"]
-  },
-  {
-    id: "margrietje-vanflaesbeck",
-    name: "Margrietje Barents VanFlaesbeck (1658–1702)",
-    branch: "Losie",
-    parents: [],
-    spouses: ["laurens-koeck"],
-    children: ["grietje-kouk"]
-  },
-  {
-    id: "grietje-kouk",
-    name: "Grietje Margaret Kouk",
-    branch: "Losie",
-    parents: ["laurens-koeck", "margrietje-vanflaesbeck"],
-    spouses: ["simon-losee"],
-    children: ["laurens-losee"]
-  },
-  {
-    id: "simon-losee",
-    name: "Simon Losee",
-    branch: "Losie",
-    parents: [],
-    spouses: ["grietje-kouk"],
-    children: ["laurens-losee"]
-  },
-  {
-    id: "laurens-losee",
-    name: "Laurens Losee (1705–1754)",
-    branch: "Losie",
-    parents: ["simon-losee", "grietje-kouk"],
-    spouses: ["rachael-delange"],
-    children: ["jan-losee"]
-  },
-  {
-    id: "rachael-delange",
-    name: "Rachael DeLange",
-    branch: "Losie",
-    parents: [],
-    spouses: ["laurens-losee"],
-    children: ["jan-losee"]
-  },
-  {
-    id: "jan-losee",
-    name: "Jan “John L.” Losee (1738–1788)",
-    branch: "Losie",
-    parents: ["laurens-losee", "rachael-delange"],
-    spouses: ["mary-renny"],
-    children: ["lawrence-losee"]
-  },
-  {
-    id: "mary-renny",
-    name: "Mary Renny",
-    branch: "Losie",
-    parents: [],
-    spouses: ["jan-losee"],
-    children: ["lawrence-losee"]
-  },
-  {
-    id: "lawrence-losee",
-    name: "Lawrence Losee (1765–1820)",
-    branch: "Losie",
-    parents: ["jan-losee", "mary-renny"],
-    spouses: ["jane-vanvoorhees"],
-    children: ["john-wesley-losie"]
-  },
-  {
-    id: "jane-vanvoorhees",
-    name: "Jane Van Voorhees",
-    branch: "Losie",
-    parents: [],
-    spouses: ["lawrence-losee"],
-    children: ["john-wesley-losie"]
-  },
-  {
-    id: "john-wesley-losie",
-    name: "John Wesley Losie (1797–1865)",
-    branch: "Losie",
-    parents: ["lawrence-losee", "jane-vanvoorhees"],
-    spouses: ["eleanor-waite"],
-    children: ["george-leo-losie"]
-  },
-  {
-    id: "eleanor-waite",
-    name: "Eleanor Waite",
-    branch: "Losie",
-    parents: [],
-    spouses: ["john-wesley-losie"],
-    children: ["george-leo-losie"]
-  },
-
-  // 🔹 George Leo Losie & Esther Alice Bearss
-
-  {
-    id: "george-leo-losie",
-    name: "George Leo Losie (1828–1880)",
-    branch: "Losie",
-    parents: ["john-wesley-losie", "eleanor-waite"],
-    spouses: ["esther-bearss"],
-    children: [
-      "sarah-losie",
-      "john-e-losie",
-      "asenath-ann-losie",
-      "bertha-alma-losie",
-      "lizanna-a-losie",
-      "anna-a-losie",
-      "ellen-a-losie",
-      "elon-albert-losie",
-      "earl-judson-losie"
-    ]
-  },
-  {
-    id: "esther-bearss",
-    name: "Esther Alice Bearss (1830–1918)",
-    branch: "Bearss",
-    parents: ["ephraim-bearss", "sarah-roszell"],
-    spouses: ["george-leo-losie"],
-    children: [
-      "sarah-losie",
-      "john-e-losie",
-      "asenath-ann-losie",
-      "bertha-alma-losie",
-      "lizanna-a-losie",
-      "anna-a-losie",
-      "ellen-a-losie",
-      "elon-albert-losie",
-      "earl-judson-losie"
-    ]
-  },
-
-  // Children of George Leo & Esther
-  {
-    id: "sarah-losie",
-    name: "Sarah E. Losie (1854–1858)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "john-e-losie",
-    name: "John E. Losie (1856–1858)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "asenath-ann-losie",
-    name: "Asenath Ann Losie (1858–1880)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "bertha-alma-losie",
-    name: "Bertha Alma Losie (1860–1900)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "lizanna-a-losie",
-    name: "Lizanna A. Losie (1862–1880)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "anna-a-losie",
-    name: "Anna A. (Lizanna) Losie (1862–1930+)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "ellen-a-losie",
-    name: "Ellen A. Losie (1865–?)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-  {
-    id: "elon-albert-losie",
-    name: "Elon Albert Losie (1865–1941)",
-    branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: [],
-    children: []
-  },
-
-  // 🔹 Earl Judson Losie & Ethel Wood
 
   {
     id: "earl-judson-losie",
-    name: "Earl Judson Losie (1867–1938)",
+    name: "Earl Judson Losie",
     branch: "Losie",
-    parents: ["george-leo-losie", "esther-bearss"],
-    spouses: ["ethel-wood"],
+    parents: [],
+    spouses: ["margaret-bearss"],
     children: [
       "earl-judson-losie-jr",
       "george-malcom-losie",
-      "charles-ferris-losie
+      "charles-ferris-losie"
+    ]
+  },
+
+  {
+    id: "margaret-bearss",
+    name: "Margaret Bearss",
+    branch: "Bearss",
+    parents: [],
+    spouses: ["earl-judson-losie"],
+    children: [
+      "earl-judson-losie-jr",
+      "george-malcom-losie",
+      "charles-ferris-losie"
+    ]
+  },
+
+  {
+    id: "earl-judson-losie-jr",
+    name: "Earl Judson Losie Jr.",
+    branch: "Losie",
+    parents: ["earl-judson-losie", "margaret-bearss"],
+    spouses: ["betty-jo-losie"],
+    children: ["david-losie", "karen-losie"]
+  },
+
+  {
+    id: "betty-jo-losie",
+    name: "Betty Jo Losie",
+    branch: "Losie",
+    parents: [],
+    spouses: ["earl-judson-losie-jr"],
+    children: ["david-losie", "karen-losie"]
+  },
+
+  {
+    id: "david-losie",
+    name: "David Losie",
+    branch: "Losie",
+    parents: ["earl-judson-losie-jr", "betty-jo-losie"],
+    spouses: [],
+    children: []
+  },
+
+  {
+    id: "karen-losie",
+    name: "Karen Losie",
+    branch: "Losie",
+    parents: ["earl-judson-losie-jr", "betty-jo-losie"],
+    spouses: [],
+    children: []
+  },
+
+  {
+    id: "george-malcom-losie",
+    name: "George Malcom Losie",
+    branch: "Losie",
+    parents: ["earl-judson-losie", "margaret-bearss"],
+    spouses: ["shirley-losie"],
+    children: ["steven-losie", "linda-losie"]
+  },
+
+  {
+    id: "shirley-losie",
+    name: "Shirley Losie",
+    branch: "Losie",
+    parents: [],
+    spouses: ["george-malcom-losie"],
+    children: ["steven-losie", "linda-losie"]
+  },
+
+  {
+    id: "steven-losie",
+    name: "Steven Losie",
+    branch: "Losie",
+    parents: ["george-malcom-losie", "shirley-losie"],
+    spouses: [],
+    children: []
+  },
+
+  {
+    id: "linda-losie",
+    name: "Linda Losie",
+    branch: "Losie",
+    parents: ["george-malcom-losie", "shirley-losie"],
+    spouses: [],
+    children: []
+  },
+
+  {
+    id: "charles-ferris-losie",
+    name: "Charles Ferris Losie",
+    branch: "Losie",
+    parents: ["earl-judson-losie", "margaret-bearss"],
+    spouses: ["joan-losie"],
+    children: ["bruce-losie", "susan-losie"]
+  },
+
+  {
+    id: "joan-losie",
+    name: "Joan Losie",
+    branch: "Losie",
+    parents: [],
+    spouses: ["charles-ferris-losie"],
+    children: ["bruce-losie", "susan-losie"]
+  },
+
+  {
+    id: "bruce-losie",
+    name: "Bruce Losie",
+    branch: "Losie",
+    parents: ["charles-ferris-losie", "joan-losie"],
+    spouses: [],
+    children: []
+  },
+
+  {
+    id: "susan-losie",
+    name: "Susan Losie",
+    branch: "Losie",
+    parents: ["charles-ferris-losie", "joan-losie"],
+    spouses: [],
+    children: []
+  }
+
+];
